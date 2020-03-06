001d2b07702830812e0144046259adfc222d553c
imadlaouani <55441738+imadlaouani@users.noreply.github.com>
2020-03-06 19:17:40 +0100

    home
    modif du home

--- PT/views.py
+++ PT/views.py
@@ -20,21 +20,26 @@ def home(request):
     enchere_gagnees =[]
     encheres = []
     if request.user.is_authenticated:
+        # Pour un vendeur ses ventes
         for i in Produit.objects.filter(vendeurFK=request.user.vendeur,dateFin__lte = timezone.now(),totalEnchere__gt=0):
             ventes.append(i)
+        # ses invendus
         for j in Produit.objects.filter(vendeurFK=request.user.vendeur,dateFin__lte = timezone.now(),totalEnchere=0):
             invendus.append(j)
-        for k in Produit.objects.filter(vendeurFK=request.user.vendeur):
+        # ses annonces en cours
+        for k in Produit.objects.filter(vendeurFK=request.user.vendeur,dateFin__gt = timezone.now()):
             mes_annonces.append(k)
-        
+        # Pour un acheteur ses encheres remportées       
         for a in Produit.objects.filter(acheteurFK=request.user.acheteur,dateFin__lte=timezone.now()):
             enchere_gagnees.append(a)
+        # ses encheres
         for c in Enchere.objects.filter(acheteurFK=request.user.acheteur):
             encheres.append(c.produitFK)
 
         # ensemble des produits auxquels l'acheteur a enchéris et dont l'enchère est terminée 
-        for b in Produit.objects.filter(numProduit_in=encheres,dateFin__lte=timezone.now()):
-            produits_encheris.append(b)
+        """for k in range(0,len(encheres)):
+            for b in Produit.objects.filter(numProduit=encheres[k],dateFin__lte=timezone.now()):
+                produits_encheris.append(b)"""
 
     return render(request, 'home.html',locals())
 
